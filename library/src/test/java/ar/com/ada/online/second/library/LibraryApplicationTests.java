252ddf3603410c4d9d996ec3b428f44672ff89bc
LI <ayalilian2018@gmail.com>
2021-04-18 23:47:42 -0300

    see

---
+++ library/src/test/java/ar/com/ada/online/second/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.online.second.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
